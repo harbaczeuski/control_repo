eeb2be59449f0ac2ef68e87174af53f372465335
harbaczeuski <42937704+harbaczeuski@users.noreply.github.com>
2022-02-09 13:06:40 +0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,5 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.master.vm':}
-  dockeragent::node {'db.master.vm':}
+  dockeragent::node {'web.puppet.vm':}
+  dockeragent::node {'db.puppet.vm':}
 }
