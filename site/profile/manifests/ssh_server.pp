1e2ddfb04e4dd3edaa2ea34a243eb76ae340a232
harbaczeuski <42937704+harbaczeuski@users.noreply.github.com>
2022-02-10 10:51:27 +0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -11,5 +11,17 @@ class profile::ssh_server {
 		user   => 'root',
 		type   => 'ssh-rsa',
 		key    => 'SHA256:ULdEatzF92Y41hfZYR/6F1Zma0mssmATMLJdhchQyWU',
-	}  
+	}
+	ssh_authorized_key { 'root@db.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'SHA256:YJhxp2c9MeAbU2jX8DD6IV1EEAkFTM+IAtQyXUrrYsY',
+	}
+	ssh_authorized_key { 'root@web.puppet.vm':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'SHA256:hzRIp9HIUrVcy8cXze+jMGzOMz7faMnbi0JCBd3gCU4',
+	}
 }
